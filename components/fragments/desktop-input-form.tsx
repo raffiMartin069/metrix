b44ba26bd9d4223921158901a68a0d610d4faa38
rafael <raffimartin069@gmail.com>
2026-04-04 02:50:55 +0000

    @chore: successfully migrated to echarts for ehanced controls

--- components/fragments/desktop-input-form.tsx
+++ components/fragments/desktop-input-form.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Plus, Trash2, Table2, Info, Send, RefreshCcwDotIcon } from "lucide-react";
 import HypothesisInput from "./hypothesis-input";
